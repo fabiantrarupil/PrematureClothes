a5e5756bb9a6e8dee0346e3133b4828d75fc7f1a
FTWebDeveloper <ftwebdeveloper@gmail.com>
2026-03-02 18:11:44 -0300

    Fix: cambio en usercontext

--- premature-clothes-frontend/src/main.jsx
+++ premature-clothes-frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 👈 Importante
-import { ProductProvider } from './context/ProductContext' // 👈 Importante
+import { BrowserRouter } from 'react-router-dom'
+import { ProductProvider } from './context/ProductContext'
+import UserProvider from './context/UserContext' // 👈 1. Importamos el nuevo proveedor
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,9 +10,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ProductProvider>
-        <App />
-      </ProductProvider>
+      <UserProvider>
+        <ProductProvider>
+          <App />
+        </ProductProvider>
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>,
 )
